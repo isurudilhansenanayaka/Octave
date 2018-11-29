da4181f9f45fe43ce920a9d61f8edc0d855b7cd0
isurudilhansenanayaka <iddmdilhan@gmail.com>
2018-11-29 22:28:14 +0530

    R problem

--- problem.R
+++ problem.R
@@ -4,4 +4,15 @@ tree_data <- datasets::trees
 MOE<- qt(.975,length(tree_data$Height)-1)*sd(tree_data$Height)/sqrt(length(tree_data$Height))
 CI<-mean(tree_data$Height)+c(-MOE,MOE)
 
+#c
+mean_girth = mean(tree_data$Girth)
+Type = c()
+for(i in 1:length(tree_data$Girth)){
+  if(tree_data$Girth[i]>=mean_girth)
+    Type[i]="A"
+  else
+    Type[i]="B"
+}
 
+tree_data=cbind(tree_data, Type)
+tree_data
